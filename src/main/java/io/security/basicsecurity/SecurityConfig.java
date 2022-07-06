f0cf5e0b5192f679cb7eb555d038b636ca6df181
지영진 <youngjin.jee@nhnsoft.com>
2022-07-06 17:58:34 +0900

    feat: WebSecurityConfigurerAdapter가 deprecated됨에 따라 filterChain Bean을 생성해서 해결

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -4,50 +4,52 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+  @Bean
+  protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
         .authorizeRequests()
         .anyRequest().authenticated();
     http
-        .formLogin()
-        .loginPage("/loginPage")
-        .defaultSuccessUrl("/")
-        .failureUrl("/login")
-        .usernameParameter("userId")
-        .passwordParameter("passwd")
-        .loginProcessingUrl("/login_proc")
-        .successHandler(new AuthenticationSuccessHandler() {
+        .formLogin();
+    http
+        .logout()
+        .logoutUrl("/logout")
+        .logoutSuccessUrl("/loginPage")
+        .addLogoutHandler(new LogoutHandler() {
           @Override
-          public void onAuthenticationSuccess(HttpServletRequest request,
-              HttpServletResponse response, Authentication authentication)
-              throws IOException, ServletException {
-            System.out.println("authentication: " + authentication.getName());
-            response.sendRedirect("/");
+          public void logout(HttpServletRequest request, HttpServletResponse response,
+              Authentication authentication) {
+            HttpSession session = request.getSession();
+            session.invalidate();
           }
         })
-        .failureHandler(new AuthenticationFailureHandler() {
+        .logoutSuccessHandler(new LogoutSuccessHandler() {
           @Override
-          public void onAuthenticationFailure(HttpServletRequest request,
-              HttpServletResponse response, AuthenticationException exception)
-              throws IOException, ServletException {
-            System.out.println("exception: " + exception.getMessage());
-            response.sendRedirect("/loginPage");
+          public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
+              Authentication authentication) throws IOException, ServletException {
+            response.sendRedirect("/login");
           }
         })
-        .permitAll();
+          .deleteCookies("remember-me")
+        .and()
+        .rememberMe()
+        .rememberMeParameter("remember")
+        .tokenValiditySeconds(3600)
+    ;
+    return http.build();
   }
 }
