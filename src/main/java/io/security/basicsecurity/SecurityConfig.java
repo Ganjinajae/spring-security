ac455daa515d8d0838c773a8068f388a6e8789b9
지영진 <wldudwls12@gmail.com>
2023-01-28 14:37:26 +0900

    feat: 인가 api - 권한 설정 표현식
    * 선언적 방식에 대해서 실습

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,17 +15,33 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
   @Bean
+  public UserDetailsManager users() {
+    UserDetails user = User.builder()
+            .username("user")
+            .password("{noop}1111")
+            .roles("USER")
+            .build();
+    UserDetails sys = User.builder()
+            .username("user")
+            .password("{noop}1111")
+            .roles("SYS", "USER")
+            .build();
+    UserDetails admin = User.builder()
+            .username("user")
+            .password("{noop}1111")
+            .roles("ADMIN", "SYS", "USER") // role hierarchy 기능 통하면 이렇게 안 해도 됨
+            .build();
+    return new InMemoryUserDetailsManager(user, sys, admin);
+  }
+
+  @Bean
   protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
-        .authorizeRequests()
-        .anyRequest().authenticated();
-    http
-        .formLogin();
-    http
-        .rememberMe();
-    http
-        .sessionManagement()
-        .sessionFixation().changeSessionId() ;
+            .authorizeRequests()
+            .antMatchers("/user").hasRole("USER")
+            .antMatchers("/admin/pay").hasRole("ADMIN") // 구체적인 범위가 위에 와있어야함, 아래와 순서 바뀌면 sys권한으로 admin/pay 접근 가능함
+            .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
+            .anyRequest().authenticated();
 
     return http.build();
   }
